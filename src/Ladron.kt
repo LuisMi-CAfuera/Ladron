f6dedfecc1cb528997f1e2c30a0490b46fd4fca0
LuisMi-CAfuera <luibra861@alu.centroafuera.es>
2022-10-20 12:57:37 +0200

    Ejercicio Mochila commit 1

--- src/Ladron.kt
+++ src/Ladron.kt
@@ -1,5 +1,5 @@
 class Mochila{
-    var pesoMax : Int = 0
+    var pesoMax : Int = 10
 
     constructor(pesoMax: Int) {
         this.pesoMax = pesoMax
@@ -11,11 +11,34 @@ class Mochila{
 class Objetos{
     var valor : Int = 0
     var peso : Int = 0
+
+    constructor(valor: Int, peso: Int) {
+        this.valor = valor
+        this.peso = peso
+    }
+    constructor(){
+
+    }
 }
 
 
-
+//Valor entre peso
 
 fun main(){
+
+    var mochila : ArrayList<Mochila> = arrayListOf<Mochila>()
+    var art = arrayOf(Objetos(30,3),Objetos(20,7),Objetos(20,3),Objetos(50,4),Objetos(30,8))
+    var art2 : ArrayList<Float> = arrayListOf<Float>()
+    var cont = 0
+
+        art.forEach {
+            art2[cont]= (it.valor/it.peso).toFloat()
+            cont++
+        }
+
+
+    art2.forEach{
+        print(it)
+    }
 
 }
